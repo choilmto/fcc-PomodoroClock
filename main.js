499370105e3bc1a70c19e7504c7a1c9bdcee3fc4
choilm <choilmto@gmail.com>
2018-04-20 21:39:11 -0400

    Fix count function.

--- main.js
+++ main.js
@@ -1,54 +1,89 @@
-const add = (total) => {
-  return total + 1;
-};
-const less = (total) => {
-  return (total - 1) < 1 ? 1 : total - 1;
-};
+let connect = {
+  count1: "total1",
+  count2: "total2"
+}
+let other = {
+  count1: "count2",
+  count2: "count1"
+}
 const count = function () {
-  this.counter = (this.counter + 1) % (60 * (this.total1 + this.total2));
+  if (this.state !== this[this.state].setLessASecond(this.state)) {
+    this[this.state] = null;
+    this.state = other[this.state];
+    this[this.state] = new timeObjectClass(this[connect[this.state]]);
+  }
+
 };
-const format = (amt) => {
-  let afterColon = amt % 60;
-  let prefix = (afterColon < 10) ? "0" : "";
-  return Math.trunc(amt / 60) + ":" + prefix + afterColon;
+class timeObjectClass {
+  constructor (obj) {
+    this.min = obj.min;
+    this.sec = obj.sec;
+  }
+  setLessASecond (st) {
+    if (this.sec === 0) {
+      if (this.min > 0) {
+        this.min--;
+        this.sec = 60;
+      } else {
+        return other[st];
+      }
+    }
+    this.sec--;
+    return st;
+  }
+  setMoreAMin () {
+    this.min++;
+  }
+  setLessAMin () {
+    this.min = (this.min > 1) ? (this.min - 1) : 1;
+  }
+  get isZero () {
+    return ((this.min === 0) && (this.sec === 0));
+  }
+  get getStr () {
+    return this.min + ":" + ((this.sec < 10) ? "0" : "") + this.sec;
+  }
 }
 let app = new Vue ({
   el: "#app",
   data: {
-    total1: 5,
-    total2: 25,
+    total1: new timeObjectClass({min: 5, sec: 0}),
+    total2: new timeObjectClass({min: 25, sec: 0}),
     isOn: false,
     controller: null,
-    counter: 0
+    state: "",
+    count1: null,
+    count2: null
   },
   methods: {
-    add : add,
-    less: less,
+    add1: function () {if (!this.isOn) {this.total1.setMoreAMin();}},
+    less1: function () {if (!this.isOn) {this.total1.setLessAMin();}},
+    add2: function () {if (!this.isOn) {this.total2.setMoreAMin();}},
+    less2: function () {if (!this.isOn) {this.total2.setLessAMin();}},
     count: count
   },
   computed: {
     show1: function () {
-      if (this.isOn && (this.counter > (this.total1 * 60))) {
-        let sec = ((this.total1 + this.total2) * 60) - this.counter;
-        return format(sec);
-      }
-      return format(this.total1 * 60);
+      return this.count1 || this.total1;
     },
     show2: function () {
-      if (this.isOn && (this.counter <= (this.total2 * 60))) {
-        let sec = (this.total2 * 60) - this.counter;
-        return format(sec);
-      }
-      return format(this.total2 * 60);
+      return this.count2 || this.total2;
     }
   },
   watch: {
     isOn: function () {
       if (this.isOn) {
+        this.state = "count2";
+        this.count2 = new timeObjectClass(this.total2);
         this.controller = setInterval(this.count.bind(this), 1000);
       } else {
         clearInterval(this.controller);
+        this[this.state] = null;
+        this.state = "";
       }
+    },
+    state: function () {
+      document.title = (this.state.toUpperCase() || "POMODORO") + " TIME!";
     }
   }
 });
